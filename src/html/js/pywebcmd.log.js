00415ff6a7059e896389c970b9f58e7f3d9cb49f
fffilo <fffilo666@gmail.com>
2015-07-01 10:00:02 +0200

    Log fix

--- src/html/js/pywebcmd.log.js
+++ src/html/js/pywebcmd.log.js
@@ -14,12 +14,14 @@
 	 */
 	window.pywebcmd.log = function(type, text) {
 		var html = type.charAt(0).toUpperCase() + type.slice(1).toLowerCase();
-		html += ':' + (new Array(10 - html.length).join('&nbsp;'));
-		html += text;
+		html += (new Array(10 - html.length).join('&nbsp;'));
+		html += window.pywebcmd.file.time(new Date().getTime() / 1000);
+		html += '&nbsp;&nbsp;';
 
 		$('<p />')
 			.attr('class', type)
 			.html(html)
+			.append(text)
 			.appendTo(window.pywebcmd.ui.log);
 
 		var log = $(window.pywebcmd.ui.log).children('p');
